29e7be7bad2382c4582dec8fe518bb0a797f2923
Erick <edu2726@gmail.com>
2018-04-16 17:58:06 -0500

    Dapper & Injector

--- Proyecto.MVC/Global.asax.cs
+++ Proyecto.MVC/Global.asax.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using log4net;
+using log4net.Core;
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +13,32 @@ using System.Web.Routing;
 
 namespace Proyecto.MVC
 {
+    public class DIConfig
+    {
+        public static void ConfigureInjector()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+            //container.Register<IU>
+            container.RegisterConditional(typeof(ILog), c => typeof(Log4NetAdapter<>).MakeGenericType(c.Consumer.ImplementationType), Lifestyle.Singleton, c => true);
+            container.Verify();
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+    }
+    public class Log4NetAdapter<T> : LogImpl
+    {
+        public Log4NetAdapter() : base(LogManager.GetLogger(typeof(T)).Logger) { }
+    }
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            DIConfig.ConfigureInjector();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
---
+++ Proyecto.Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        int Insert(T entity);
+        bool Delete(T entity);
+        bool Update(T entity);
+        IEnumerable<T> GetList();
+        //T GetById(string id);
+        T GetById(int id);
+    }
+}
